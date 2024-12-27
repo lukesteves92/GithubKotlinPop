5464225d9f8a4ac9b663e831c281ec1fb528eb37
lucas.esteves <lucas.coelho9280@gmail.com>
2024-12-27 17:37:14 -0300

    Configuring Splash Screen

--- composeApp/src/androidMain/kotlin/com/challenge/kotlinpop/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/challenge/kotlinpop/MainActivity.kt
@@ -2,14 +2,28 @@ package com.challenge.kotlinpop
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT,
+            )
+        )
+        installSplashScreen()
+        actionBar?.hide()
         setContent {
             App()
         }
